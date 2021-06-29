57e7fb1ee991806690954de61c0681702a1377a7
JackMunn <jackmunn@gmail.com>
2021-06-29 18:03:17 +0100

    Changed a bit of wording for percy test

--- cypress/integration/E2E/happyPath1.test.js
+++ cypress/integration/E2E/happyPath1.test.js
@@ -6,7 +6,7 @@ describe('Happy Path 1',  () => {
 
     it('Customer can fill in a basic quote', () => {
         //pre-fill a quote
-        cy.percySnapshot()
+        cy.percySnapshot('Empty Quick Quote Page')
         cy.prefillQuickQuoteQuestions('Jack','Munn','jackmunn@gmail.com',Cypress.todaysDate,'ABHKZ660047N99LP','CF64 5RL','11/05/1988','01234567890','CV53JBZ')
 
         //click continue
