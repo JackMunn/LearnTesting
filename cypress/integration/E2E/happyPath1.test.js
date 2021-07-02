f1164fbaafb69d6e0cc51b37d9d8e7eea3d15c7c
JackMunn <jackmunn@gmail.com>
2021-07-02 07:35:40 +0100

    Removed percy snapshots, tests still failing

--- cypress/integration/E2E/happyPath1.test.js
+++ cypress/integration/E2E/happyPath1.test.js
@@ -7,7 +7,7 @@ describe('Happy Path 1',  () => {
     it('Customer can fill in a basic quote', () => {
         //pre-fill a quote
         cy.log('Staring smoke test')
-        cy.percySnapshot('Empty Quick Quote Page')
+        // cy.percySnapshot('Empty Quick Quote Page')
         cy.prefillQuickQuoteQuestions('Jack','Munn','jackmunn@gmail.com',Cypress.todaysDate,'ABHKZ660047N99LP','CF64 5RL','11/05/1988','01234567890','CV53JBZ')
         // cy.percySnapshot('Filled Quick Quote Page (No Errors)')
 
