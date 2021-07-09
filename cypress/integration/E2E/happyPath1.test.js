1e0b5b1cf15baa4bc24b7275530a4fa2214e1d23
JackMunn <jackmunn@gmail.com>
2021-07-09 09:17:53 +0100

    Removing percy... again

--- cypress/integration/E2E/happyPath1.test.js
+++ cypress/integration/E2E/happyPath1.test.js
@@ -6,10 +6,10 @@ describe('Happy Path 1',  () => {
     it('Customer can fill in a basic quote', () => {
         
         //pre-fill a quote
-        cy.log('Staring smoke test', { widths: [375]})
-        cy.percySnapshot('Empty Quick Quote Page')
+        cy.log('Staring smoke test')
+        // cy.percySnapshot('Empty Quick Quote Page')
         cy.prefillQuickQuoteQuestions('Jack','Munn','jackmunn@gmail.com',Cypress.todaysDate,'ABHKZ660047N99LP','CF64 5RL','11/05/1988','01234567890','CV53JBZ')
-        cy.percySnapshot('Filled Quick Quote Page (No Errors)', { widths: [375]})
+        // cy.percySnapshot('Filled Quick Quote Page (No Errors)', { widths: [375]})
 
         // click continue
         cy.get('[formaction="QuickQuoteQuestionsContinue"]').click()
