863e305c002ae7083a9d5740cfd58cf0593ff38a
JackMunn <jackmunn@gmail.com>
2021-08-02 15:49:40 +0100

    changed email in happy path 2 to be valid

--- cypress/integration/E2E/happyPath2.test.js
+++ cypress/integration/E2E/happyPath2.test.js
@@ -8,7 +8,7 @@ describe('Happy Path 2',  () => {
         //pre-fill a quote
         cy.log('Staring smoke test')
         // cy.percySnapshot('Empty Quick Quote Page')
-        cy.prefillQuickQuoteQuestions('firstName','secondName','asdadadasd@gmail.com',Cypress.todaysDate,'ABHKZ660047N99LP','CF64 5RL','11/05/1950','01234567890','CV53JBZ')
+        cy.prefillQuickQuoteQuestions('firstName','secondName','jackmunn@gmail.com',Cypress.todaysDate,'ABHKZ660047N99LP','CF64 5RL','11/05/1950','01234567890','CV53JBZ')
         // cy.percySnapshot('Filled Quick Quote Page (No Errors)', { widths: [375]})
 
         // click continue
